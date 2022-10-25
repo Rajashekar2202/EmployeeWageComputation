759e19bfb42ae8387da702355de6f1ec3d1ac8c4
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-25 20:21:08 +0530

    added Employee Present or Abscent Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckAttendance checkAttendance = new CheckAttendance();
+            checkAttendance.Check();
+        }
+    }
+}
